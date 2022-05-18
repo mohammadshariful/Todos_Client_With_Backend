c6fd4d868701e8707c884c646579a2196e3648a6
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-18 21:31:08 +0600

    task completed with api implement

--- src/Pages/Home/SingleTodo.js
+++ src/Pages/Home/SingleTodo.js
@@ -2,7 +2,8 @@ import React from "react";
 import { toast } from "react-toastify";
 
 const SingleTodo = ({ todo, refetch }) => {
-  const { _id, name, description } = todo;
+  const { _id, name, description, isComplete } = todo;
+
   //handle delete
   const handleDelete = (id) => {
     const process = window.confirm("Are You sure want to delete?");
@@ -25,15 +26,38 @@ const SingleTodo = ({ todo, refetch }) => {
         });
     }
   };
+  // handle task completed
+  const handleTaskComplete = (id) => {
+    const url = `http://localhost:5000/todos/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          toast.success("Task Completed", {
+            position: toast.POSITION.TOP_CENTER,
+          });
+          refetch();
+        }
+      });
+  };
   return (
     <div className="card w-72 lg:w-96  lg:max-w-lg bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="text-lg font-bold">
           Task: <span className="text-primary">{name}</span>
         </h2>
-        <p>{description}</p>
+        <p className={isComplete && "line-through"}>{description}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary  btn-sm text-white ">
+          <button
+            disabled={isComplete}
+            onClick={() => handleTaskComplete(_id)}
+            className="btn btn-primary  btn-sm text-white "
+          >
             Complete
           </button>
           <button
